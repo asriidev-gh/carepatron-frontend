c2e188c814e77afaf0a5f46a767afce8cff3bb41
Alejandro Radam II <alejandro.radam@eastvantage-europe.com>
2023-08-31 17:02:40 +0800

    feat: apply theme provider to customize mui component's color

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,18 +1,36 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import DataProvider from "./store/DataProvider";
-import Clients from "./pages/Clients";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import DataProvider from './store/DataProvider';
+import Clients from './pages/Clients';
+import { green } from '@mui/material/colors';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+	components: {
+		MuiStepIcon: {
+			styleOverrides: {
+				root: {
+					'&.Mui-completed': {
+						color: green[800],
+					},
+				},
+			},
+		},
+	},
+});
 
 export default function App() {
-  return (
-    <div className="App">
-      <DataProvider>
-        <Routes>
-          <Route path="/" element={<Clients />} />
-          <Route path="/Clients" element={<Clients />} />
-        </Routes>
-      </DataProvider>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<ThemeProvider theme={theme}>
+				<DataProvider>
+					<Routes>
+						<Route path='/' element={<Clients />} />
+						<Route path='/Clients' element={<Clients />} />
+					</Routes>
+				</DataProvider>
+			</ThemeProvider>
+		</div>
+	);
 }
